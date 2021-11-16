a6b1a5a8cd25107cf7b180a3dc0a9befd0843f92
ahcene <ahcene.dahoumane@u-psud.fr>
2021-11-16 01:17:51 +0100

    firstModification

---
+++ src/frog/Frog.java
@@ -0,0 +1,27 @@
+package frog;
+
+import gameCommons.Game;
+import gameCommons.IFrog;
+import util.Case;
+import graphicalElements.FroggerGraphic;
+
+import java.awt.*;
+
+public class Frog implements IFrog   {
+	private Game game;
+	private Case myFrog;
+	enum Direction sens;
+
+
+
+
+		myFrog.(Color.green);
+
+
+		getPosition();
+	 	getDirection();
+	 	move(key);
+
+
+
+}
