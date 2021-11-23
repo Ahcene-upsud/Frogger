c59f96beac5e2bd202722406146d2a0635554648
ahcene.dahoumane <arezkiomar82@gmail.com>
2021-11-23 12:07:21 +0100

    finParti1

--- src/frog/Frog.java
+++ src/frog/Frog.java
@@ -16,40 +16,41 @@ import java.awt.event.KeyListener;
 public class Frog implements IFrog   {
 	private Game game;
 	private Direction sens;
-	private String ad;
+	private Position ad;
 	private Case p;
 
 	public Frog(Game newGame){
 		game = newGame ;
-		sens = Direction.up;
-		p = new Case (game.width/2 , game.height );
-}
-
-
-/*public  Frog(Game game , Case p ,  Direction sens )	{
-	this.game = game;
-	this.sens = sens;
-	this.p=p;*/
-
-}
-
-	public Case deplacementFrog(Case p){
-			getPosition();
-}
-public void move(Direction key){
-	switch (key) {
-		case Direction.up:
-			p.ord+1.getDirection();
-			break;
-		case Direction.down:
-			p.ord-1.getDirection();
-			break;
-		case Direction.right:
-			p.absc-1.getDirection();
-			break;
-		case Direction.left:
-			p.absc+1.getDirection();
+		//sens = Direction.up;
+		p = new Case (game.width/2 , 0);
 	}
-}
 
+	public Case getPosition(){
+		return  p;
+	}
+
+	/*public  Frog(Game game , Case p ,  Direction sens )	{
+		this.game = game;
+		this.sens = sens;
+		this.p=p;*/
+
+
+
+	public Direction getDirection() {
+		return getDirection();
+	}
+	public void move(Direction key){
+		switch (key) {
+			case up:
+				p = new Case (p.absc ,p.ord+1);
+				break;
+			case down:
+				p = new Case (p.absc ,p.ord-1);
+				break;
+			case right:
+				p = new Case (p.absc+1 ,p.ord);
+				break;
+			case left:
+				p = new Case (p.absc-1 ,p.ord);		}
+	}
 }
